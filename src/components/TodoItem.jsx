ac8f8f46221ab42499258817a59d4e4d7a92f0ea
oscar <oscar.vasquez@zentro.com.mx>
2024-05-13 17:44:39 -0600

    debug

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,8 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const useFetch = (url: string) =>{
     const [data, setData] = useState(null);
-    // 4:32 / 11:22
+    const [loading, setLoading] = useState(second);
+
+    useEffect(() => {
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            setData(data)
+            setLoading(false)
+        })
+    }, [url]);
+
+    return{
+        data,
+        loading
+    };
+    
 }
 
 export const TodoItem = () => {
@@ -10,3 +25,21 @@ export const TodoItem = () => {
     <div>TodoItem</div>
   )
 }
+
+export const TodoitemWitCustomHook = () => {
+
+    const {data} = useFetch(
+        "https://jsonplaceholder.typicode.com/todos/1"
+    );
+
+    if(loading){
+        return <p>Loading...</p>
+    }
+
+    return(
+        <div>
+            <p>Title {data?.title}</p>
+        </div>
+    );
+
+};
