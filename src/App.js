9e35f8f8b3e7c5537aefc4f511b14ed65d6b8384
oscar <oscar.vasquez@zentro.com.mx>
2024-05-13 16:04:17 -0600

    debug

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
   return (
     <div className="App">
      {/* https://www.youtube.com/watch?v=VJlANS3IPxg */}
+     {/* https://www.youtube.com/watch?v=K4vCTeKKCkU  */}
+     {/* https://www.youtube.com/watch?v=odpX-rJ_R4Q */}
      <TodoItem/>
     </div>
   );
