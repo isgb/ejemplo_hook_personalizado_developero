bf649aa96be65184cf8e0e8c16fbe82908787007
oscar <oscar.vasquez@zentro.com.mx>
2024-05-13 16:03:28 -0600

    debug

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { TodoItem } from './components/TodoItem';
 
 function App() {
   return (
     <div className="App">
      {/* https://www.youtube.com/watch?v=VJlANS3IPxg */}
+     <TodoItem/>
     </div>
   );
 }
---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const useFetch = (url: string) =>{
+    const [data, setData] = useState(null);
+    // 4:32 / 11:22
+}
+
+export const TodoItem = () => {
+  return (
+    <div>TodoItem</div>
+  )
+}
